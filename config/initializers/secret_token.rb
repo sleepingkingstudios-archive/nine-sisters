40a4f44006c5f44217d14f5ecfd4d3402d0bd45d
Rob Smith <sleepingkingstudios@gmail.com>
2011-12-04 14:28:23 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NineSisters::Application.config.secret_token = '56f452d890af785b3edff44d5a6ebc17a10a686d45a1f89a0309bb96f89971d39cf3623e653ac751db29a0ef775aafd7243a4d160919c410a718d922e3db24a2'
