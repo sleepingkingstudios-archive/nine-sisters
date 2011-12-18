125838e72b52b23c8d6938b178f2c1a572c8d35c
Rob Smith <sleepingkingstudios@gmail.com>
2011-12-18 08:07:16 -0500

    Added caching to Setting model

--- app/models/setting.rb
+++ app/models/setting.rb
@@ -1,9 +1,13 @@
 class Setting < ActiveRecord::Base
   class << self
     def [](key)
-      (setting = self.find_by_key(key)) ? setting.value : nil
+      (@@settings ||= Hash.new)[key] || (setting = self.find_by_key(key)) ? setting.value : nil
     end # method []
   end # class << self
+  
+  after_save do |setting|
+    (@@settings ||= Hash.new)[setting.key] = setting.value
+  end # after_save
   
   validates :key,   :presence => :true, :uniqueness => :true
   validates :value, :presence => :true
